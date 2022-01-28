4db5a679917b2bb8e37ef30294472a502ab9e372
Meet Rajput <rajputmeet10@outlook.com>
2022-01-28 15:13:53 +0530

    listItemsProvided

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "./Header";
 function App() {
+  let inputField=""
+  const [item,setItem]=useState("");
+  const [itemList,setItemList]=useState([]);
+  const addItem=(event)=>{
+    setItem(event.target.value);
+  };
+  const listOfItems=()=>{
+    setItemList((oldItems)=>{
+      return [...oldItems, item];
+    });
+    this.mainInput.value="";
+  }
   return (
     <div>
       <div className="main_div">
         <div className="center_div">
           <br></br>
           <Header/>
-          <input type="text"placeholder='Enter item...'/>
-          <button className='btn'>+</button>
+          <input id="mainInput"onChange={addItem}type="text"placeholder='Enter item...'/>
+          <button className="btn" onClick={listOfItems}>+</button>
+          <div className='actualList'>
+            <ol>
+              {
+                itemList.map((itemVal)=>{
+                  return <li> {itemVal} </li>
+                })
+              }
+            </ol>
+          </div>
         </div>
       </div>
     </div>
